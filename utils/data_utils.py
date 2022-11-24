f9279729e524fac6a2a6a1022223afea640e3d0d
Nguyen Van Tam <tamnvhustcc@gmail.com>
2022-11-24 11:09:45 +0700

    Initial commit

---
+++ utils/data_utils.py
@@ -0,0 +1,164 @@
+import json
+import os
+import tensorflow as tf
+# import tensorflow_datasets as tfds
+from PIL import Image
+import numpy as np
+from tfrecord_utils import get_tfrecord_dataset
+
+
+# def preprocessing(image_data, final_height, final_width, augmentation_fn=None, evaluate=False):
+#     """Image resizing operation handled before batch operations.
+#     inputs:
+#         image_data = tensorflow dataset image_data
+#         final_height = final image height after resizing
+#         final_width = final image width after resizing
+
+#     outputs:
+#         img = (final_height, final_width, channels)
+#         gt_boxes = (gt_box_size, [y1, x1, y2, x2])
+#         gt_labels = (gt_box_size)
+#     """
+#     img = image_data["image"]
+#     gt_boxes = image_data["objects"]["bbox"]
+#     gt_labels = tf.cast(image_data["objects"]["label"] + 1, tf.int32)
+#     img = tf.image.convert_image_dtype(img, tf.float32)
+#     img = tf.image.resize(img, (final_height, final_width))
+#     if evaluate:
+#         not_diff = tf.logical_not(image_data["objects"]["is_difficult"])
+#         gt_boxes = gt_boxes[not_diff]
+#         gt_labels = gt_labels[not_diff]
+#     if augmentation_fn:
+#         img, gt_boxes = augmentation_fn(img, gt_boxes)
+#     return img, gt_boxes, gt_labels
+def preprocessing(image_data, final_height, final_width, augmentation_fn=None, evaluate=False):
+    example_fmt = {
+        'image/height': tf.io.FixedLenFeature([], tf.int64),
+        'image/width': tf.io.FixedLenFeature([], tf.int64),
+        'image/encoded': tf.io.FixedLenFeature([], tf.string),
+        'image/object/bbox/xmin': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/xmax': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/ymin': tf.io.VarLenFeature(tf.float32),
+        'image/object/bbox/ymax': tf.io.VarLenFeature(tf.float32),
+        'image/object/class/text': tf.io.VarLenFeature(tf.string),
+        'image/object/class/label': tf.io.VarLenFeature(tf.int64),
+    }
+    parsed_example = tf.io.parse_single_example(image_data, example_fmt)
+    image = tf.image.decode_jpeg(parsed_example['image/encoded'])
+    image_height = parsed_example['image/height']
+    image_width = parsed_example['image/width']
+    image = tf.reshape(image, (image_height, image_width, 3))
+    image = tf.cast(tf.image.resize(image, (final_height, final_width)), tf.float32)
+
+    xmins = tf.sparse.to_dense(parsed_example['image/object/bbox/xmin'])
+    ymins = tf.sparse.to_dense(parsed_example['image/object/bbox/ymin'])
+    xmaxs = tf.sparse.to_dense(parsed_example['image/object/bbox/xmax'])
+    ymaxs = tf.sparse.to_dense(parsed_example['image/object/bbox/ymax'])
+    labels = tf.cast(tf.sparse.to_dense(
+        parsed_example['image/object/class/label']), tf.int32)
+
+    bboxes = tf.stack([
+        ymins,
+        xmins,
+        ymaxs,
+        xmaxs,
+    ], 1)
+    if augmentation_fn:
+        image, bboxes = augmentation_fn(image, bboxes)
+
+    return image, bboxes, labels
+
+
+# def get_dataset(name, split, data_dir="~/tensorflow_datasets"):
+#     """Get tensorflow dataset split and info.
+#     inputs:
+#         name = name of the dataset, voc/2007, voc/2012, etc.
+#         split = data split string, should be one of ["train", "validation", "test"]
+#         data_dir = read/write path for tensorflow datasets
+
+#     outputs:
+#         dataset = tensorflow dataset split
+#         info = tensorflow dataset info
+#     """
+#     assert split in ["train", "train+validation", "validation", "test"]
+#     dataset, info = tfds.load(name, split=split, data_dir=data_dir, with_info=True)
+#     return dataset, info
+def get_dataset(image_dir, ann_file, tfrecord_file):
+    return get_tfrecord_dataset(image_dir, ann_file, tfrecord_file)
+
+def get_total_item_size(info, split):
+    """Get total item size for given split.
+    inputs:
+        info = tensorflow dataset info
+        split = data split string, should be one of ["train", "validation", "test"]
+
+    outputs:
+        total_item_size = number of total items
+    """
+    assert split in ["train", "train+validation", "validation", "test"]
+    if split == "train+validation":
+        return info.splits["train"].num_examples + info.splits["validation"].num_examples
+    return info.splits[split].num_examples
+
+def get_labels(info):
+    """Get label names list.
+    inputs:
+        info = tensorflow dataset info
+
+    outputs:
+        labels = [labels list]
+    """
+    return info.features["labels"].names
+
+def get_custom_imgs(custom_image_path):
+    """Generating a list of images for given path.
+    inputs:
+        custom_image_path = folder of the custom images
+    outputs:
+        custom image list = [path1, path2]
+    """
+    img_paths = []
+    for path, dir, filenames in os.walk(custom_image_path):
+        for filename in filenames:
+            img_paths.append(os.path.join(path, filename))
+        break
+    return img_paths
+
+def custom_data_generator(img_paths, final_height, final_width):
+    """Yielding custom entities as dataset.
+    inputs:
+        img_paths = custom image paths
+        final_height = final image height after resizing
+        final_width = final image width after resizing
+    outputs:
+        img = (final_height, final_width, depth)
+        dummy_gt_boxes = (None, None)
+        dummy_gt_labels = (None, )
+    """
+    for img_path in img_paths:
+        image = Image.open(img_path)
+        resized_image = image.resize((final_width, final_height), Image.LANCZOS)
+        img = np.array(resized_image)
+        img = tf.image.convert_image_dtype(img, tf.float32)
+        yield img, tf.constant([[]], dtype=tf.float32), tf.constant([], dtype=tf.int32)
+
+def get_data_types():
+    """Generating data types for tensorflow datasets.
+    outputs:
+        data types = output data types for (images, ground truth boxes, ground truth labels)
+    """
+    return (tf.float32, tf.float32, tf.int32)
+
+def get_data_shapes():
+    """Generating data shapes for tensorflow datasets.
+    outputs:
+        data shapes = output data shapes for (images, ground truth boxes, ground truth labels)
+    """
+    return ([None, None, None], [None, None], [None,])
+
+def get_padding_values():
+    """Generating padding values for missing values in batch for tensorflow datasets.
+    outputs:
+        padding values = padding values with dtypes for (images, ground truth boxes, ground truth labels)
+    """
+    return (tf.constant(0, tf.float32), tf.constant(0, tf.float32), tf.constant(-1, tf.int32))

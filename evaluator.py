b0d9e8ed94f7bb68f9934e9f31f9f69e5f75d5fa
Nguyen Van Tam <tamnvhustcc@gmail.com>
2022-11-24 14:18:47 +0700

    Added evaluator

--- evaluator.py
+++ evaluator.py
@@ -13,7 +13,7 @@ class EvaluatorCalback(Callback):
         self.prior_boxes = prior_boxes
         self.hyper_params = hyper_params
 
-    def on_epoch_end(self, epoch):
+    def on_epoch_end(self, epoch, logs=None):
         ssd_decoder_model = get_decoder_model(self.model, self.prior_boxes, self.hyper_params)
         pred_bboxes, pred_labels, pred_scores = ssd_decoder_model.predict(self.eval_data, steps=self.eval_steps, verbose=1)
         stats, mAP = evaluate_predictions(self.eval_data, pred_bboxes, pred_labels, pred_scores, self.labels, self.batch_size)
